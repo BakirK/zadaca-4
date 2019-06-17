63d320336a6b0cf127755b170cf1e91ed44b4e16
Bakir Karovic <bakir.karovic@gmail.com>
2019-06-17 21:29:53 +0200

    Dodane osnovne metode i polja iz fxml-a

--- src/ba/unsa/etf/rs/zadaca4/MainController.java
+++ src/ba/unsa/etf/rs/zadaca4/MainController.java
@@ -1,4 +1,50 @@
 package ba.unsa.etf.rs.zadaca4;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MainController {
+    @FXML
+    private Tab ownersTab, vehiclesTab;
+    @FXML
+    private TableView tableOwners, tableVehicles;
+    @FXML
+    private TableColumn tableOwnersId, tableOwnersName, tableOwnersJmbg, tableVehiclesId, tableVehiclesManufacturer,
+            tableVehiclesModel, tableVehiclesChasisNumber, tableVehiclesPlateNumber;
+    @FXML
+    private Label searchVehicle, searchOwner;
+
+
+    @FXML
+    private void useSQL(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void useXML(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void addOwner(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void removeOwner(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void editOwner(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void addVehicle(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void removeVehicle(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void editVehicle(ActionEvent actionEvent) {
+    }
 }
