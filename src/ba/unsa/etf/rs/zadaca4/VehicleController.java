c62f3f0462585949ea4f95fdf78d642e45698cca
Bakir Karovic <bakir.karovic@gmail.com>
2019-06-18 19:37:51 +0200

    Implementirano zatvaranje prozora

--- src/ba/unsa/etf/rs/zadaca4/VehicleController.java
+++ src/ba/unsa/etf/rs/zadaca4/VehicleController.java
@@ -2,8 +2,10 @@ package ba.unsa.etf.rs.zadaca4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class VehicleController {
     @FXML
@@ -26,5 +28,8 @@ public class VehicleController {
 
     @FXML
     private void zatvoriProzorPropuhJe(ActionEvent actionEvent) {
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
     }
 }
