69b08c3ea9413081d97501dde2448148a7994732
Bakir Karovic <bakir.karovic@gmail.com>
2019-06-18 19:37:02 +0200

    Implementirane osnovne metode i parametri iz fxml

--- src/ba/unsa/etf/rs/zadaca4/VehicleController.java
+++ src/ba/unsa/etf/rs/zadaca4/VehicleController.java
@@ -1,6 +1,30 @@
 package ba.unsa.etf.rs.zadaca4;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class VehicleController {
+    @FXML
+    private TextField modelField;
+    @FXML
+    private TextField chasisNumberField;
+    @FXML
+    private TextField plateNumberField;
+    @FXML
+    private ComboBox manufacturerCombo;
+    @FXML
+    private ComboBox ownerCombo;
+
     public VehicleController(VehicleDAO dao, Vehicle v) {
+    }
+
+    @FXML
+    private void validateInput(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void zatvoriProzorPropuhJe(ActionEvent actionEvent) {
     }
 }
